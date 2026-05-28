22433bd070cf154074eda61e0d2d227f59862192
Jonathan Hanska Susanto <jojo12ku@gmail.com>
2026-05-28 23:48:47 +0700

    chore: update dependencies and vite config

--- beeTip/vite.config.ts
+++ beeTip/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/auth': 'http://localhost:3000',
+      '/orders': 'http://localhost:3000',
+      '/transactions': 'http://localhost:3000',
+    },
+  },
 })
